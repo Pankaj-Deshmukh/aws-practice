e1bb4ce9c9dffe98bf2c02017c9c2649bd9f72d6
Pankaj-Deshmukh <pankaj.deshmukh.2110@gmail.com>
2025-07-04 12:23:07 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import StudentTable from './pages/studentTable'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
